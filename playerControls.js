0703f14b642b249ae29410ca91afb248cda3077d
David Leaver <danzel@localhost.geek.nz>
2014-01-25 12:41:52 +1300

    Don't care what joystick controls it

--- playerControls.js
+++ playerControls.js
@@ -10,31 +10,28 @@ PlayerControls = function () {
 	var self = this;
 	//Shoulder toggle
 	gamepad.bind(Gamepad.Event.BUTTON_DOWN, function (e) {
-		if (e.gamepad.index === 0 && e.control == 'RIGHT_TOP_SHOULDER') {
+		if (e.control == 'RIGHT_TOP_SHOULDER') {
 			Events.publish('player-toggle-bigness', !player.isBig);
 		}
 	});
 
 	gamepad.bind(Gamepad.Event.AXIS_CHANGED, function (e) {
-		if (e.gamepad.index === 0) {
+		//Right stick move laser
+		if (e.axis === 'RIGHT_STICK_X') {
+			self.laserOffset.x = player.laserRange * e.value;
+			Events.publish('player-laser-target-move', self.laserOffset);
+		} else if (e.axis === 'RIGHT_STICK_Y') {
+			self.laserOffset.y = player.laserRange * e.value;
+			Events.publish('player-laser-target-move', self.laserOffset);
+		}
+		//TODO: Left stick move player
 
-			//Right stick move laser
-			if (e.axis === 'RIGHT_STICK_X') {
-				self.laserOffset.x = player.laserRange * e.value;
-				Events.publish('player-laser-target-move', self.laserOffset);
-			} else if (e.axis === 'RIGHT_STICK_Y') {
-				self.laserOffset.y = player.laserRange * e.value;
-				Events.publish('player-laser-target-move', self.laserOffset);
-			}
-			//TODO: Left stick move player
-
-			if (e.axis === 'LEFT_STICK_X') {
-				self.movementDirection.x = e.value;
-				Events.publish('player-movement-direction', self.movementDirection);
-			} else if (e.axis === 'LEFT_STICK_Y') {
-				self.movementDirection.y = e.value;
-				Events.publish('player-movement-direction', self.movementDirection);
-			}
+		if (e.axis === 'LEFT_STICK_X') {
+			self.movementDirection.x = e.value;
+			Events.publish('player-movement-direction', self.movementDirection);
+		} else if (e.axis === 'LEFT_STICK_Y') {
+			self.movementDirection.y = e.value;
+			Events.publish('player-movement-direction', self.movementDirection);
 		}
 	});
 };
