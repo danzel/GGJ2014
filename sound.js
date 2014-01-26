69a134afd7f225db8d10ad6656bd157a69158fa6
David Leaver <danzel@localhost.geek.nz>
2014-01-26 14:15:49 +1300

    cat meow on die :(

--- sound.js
+++ sound.js
@@ -1,5 +1,12 @@
 var soundResources = [
-	{ id: 'alert', src: 'sound/alert.mp3' }
+	{ id: 'alert', src: 'sound/alert.mp3' },
+	{ id: 'meow1', src: 'sound/cclsounds/kittenmeow1.mp3' },
+	{ id: 'meow2', src: 'sound/cclsounds/kittenmeow2.mp3' },
+	{ id: 'meow3', src: 'sound/cclsounds/kittenmeow3.mp3' },
+	{ id: 'meow4', src: 'sound/cclsounds/kittenmeow4.mp3' },
+	{ id: 'meow5', src: 'sound/cclsounds/kittenmeow5.mp3' },
+	{ id: 'meow6', src: 'sound/cclsounds/kittenmeow6.mp3' },
+	{ id: 'meow7', src: 'sound/cclsounds/kittenmeow7.mp3' }
 ];
 
 SafeSound = function (soundNames, delay) {
@@ -19,19 +26,32 @@ SafeSound.prototype = {
 			this.lastPlayed = now;
 		}
 	}
-}
+};
 
 
 SoundManager = {
 	init: function () {
 		var self = this;
 		this._alert = new SafeSound(['alert'], 0.2);
+		this._meow = new SafeSound([
+			'meow1',
+			'meow2',
+			'meow3',
+			'meow4',
+			'meow5',
+			'meow6',
+			'meow7'
+		], 0);
 
 		Events.create('cat-started-waiting');
 
 
 		Events.subscribe('cat-started-waiting', function () {
 			self._alert.play();
+		});
+
+		Events.subscribe('cat-died', function () {
+			self._meow.play();
 		});
 	}
 }
